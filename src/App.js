31f6f8faffd9c41cfe97862a5f88954022ad2d11
Mark Dewey <mark.t.dewey@gmail.com>
2017-10-29 11:29:08 -0400

    added profile pic

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import facePic from './images/me.jpg'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={facePic} className="face-pic" alt="Me" />
+          <h1 className="my-name">Mark Dewey</h1>
+          <h4>Software Engineer &amp; Instructor</h4>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
