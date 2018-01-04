05aee0e270bcb66ab8f3d74f69f211d7996c3a14
Mark Dewey <mark.t.dewey@gmail.com>
2018-01-03 23:21:36 -0500

    added picture and layout for projects

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // packages
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 // components css
 import './index.scss';
 
@@ -20,27 +20,40 @@ class App extends Component {
             </section>
             <h4>- Developer - Instructor - Explorer -</h4>
           </header>
-
           <section className="contact-info sub-section">
-            <a href="#contact" className="contact-button"><i class="material-icons">info</i>Just looking to contact me?</a>
+            <a href="#contact" className="contact-button">
+              <i class="material-icons">info</i>Just looking to contact me?</a>
           </section>
 
           <section className="current-status-section sub-section">
             <em className="current-status-header">Current Status:</em>
-            <div className="current-status-text">Rising and Grinding </div>
+            <div className="current-status-text">Leaving my comfort zone.</div>
           </section>
 
           <section className="projects-section section">
-            <h2>Current Notable Projects</h2>
-            <h4>view all on <i class="devicon-github-plain-wordmark colored"></i></h4>
+            <header>
+              <h2>Current Notable Projects</h2>
+            </header>
             <section className="projects-list">
-              <div className="project">St Pete.Lease</div>
-              <div className="project">student-list</div>
-              <div className="project">PathfinderTools</div>
-              <div className="project">Trainable Bot</div>
+              <div className="project st-pete-lease">
+                 <div><a href="#">St Pete.Lease</a></div>
+              </div>
+              <div className="project student-list"><div><a href="#">Tampa Bay Demo Day</a></div></div>
+              <div className="project pathfinder"><div><a href="#">PathfinderTools</a></div></div>
+              <div className="project trainable-bot"><div><a href="#">Trainable Bot</a></div></div>
             </section>
+            <footer>
+              <a
+                href="https://www.github.com/mdewey"
+                target="_blank"
+                rel="noopener noreferrer">
+                <nav>
+                  <i class="devicon-github-plain colored"></i>
+                  view github
+                </nav>
+              </a>
+            </footer>
           </section>
-
 
           <section className="section">
             <h2>Core Values</h2>
@@ -55,12 +68,16 @@ class App extends Component {
           <section className="section">
             <header>Top Skills</header>
             <section className="skills-list">
-               <section className="sub-section">languages</section>
-               <section className="sub-section">Frameworks</section>
-               <section className="sub-section">concepts</section>
+              <section className="sub-section">languages</section>
+              <section className="sub-section">Frameworks</section>
+              <section className="sub-section">concepts</section>
             </section>
             <footer>
-              view full list of skills on my <a target="_blank" rel="noopener noreferrer" href="https://mdewey.github.io/resume/">resume</a>
+              view full list of skills on my
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://mdewey.github.io/resume/">resume</a>
             </footer>
           </section>
 
